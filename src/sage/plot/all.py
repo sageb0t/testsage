7549581a33a554194338dc4818051b6c8067ebe9
William Stein <wstein@ucsd.edu>
2006-05-07 06:03:52 +0000

    [project @ log_html -- added support for plots!]

--- src/sage/plot/all.py
+++ src/sage/plot/all.py
@@ -1,3 +1,4 @@
 from plot import (Graphics, line, polygon, plot, point,
                   text, circle, disk, hue, graphics_array,
-                  list_plot, parametric_plot)
+                  list_plot, parametric_plot,
+                  is_Graphics)
