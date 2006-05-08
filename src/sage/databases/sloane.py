2851e30907208ddc47dd9f8a10ddf26aa09a3a4c
William Stein <wstein@ucsd.edu>
2006-05-08 15:57:48 +0000

    [project @ groups -- tiny fixes (and added some debuging output to sloane.py)]

--- src/sage/databases/sloane.py
+++ src/sage/databases/sloane.py
@@ -260,6 +260,8 @@ def sloane_sequence(number):
     j = t.find("</pre>")
     if i == -1 or j == -1:
         #return []
+        print s
+        print "url = ", url
         raise ValueError, "sequence '%s' not found"%number
     text = s[i+5:j].strip()
 
