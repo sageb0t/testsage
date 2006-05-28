591a3afe862a943bd858ae06ee3f88b02ee02665
William Stein <wstein@ucsd.edu>
2006-05-28 01:02:45 +0000

    [project @ mwrank initprimes -- add docs and polished ifti's patch.]

--- src/sage/libs/mwrank/all.py
+++ src/sage/libs/mwrank/all.py
@@ -4,4 +4,7 @@ Cremona's mwrank C++ library
 
 __doc_exclude = []  # to include everything
 
-from interface import mwrank_EllipticCurve, mwrank_MordellWeil, set_precision
+from interface import (mwrank_EllipticCurve, mwrank_MordellWeil,
+                       set_precision)
+
+from mwrank import initprimes as mwrank_initprimes
