63db707937aa0165029e4467e18f691d09fa640b
William Stein <wstein@ucsd.edu>
2006-06-20 03:47:12 +0000

    [project @ notebook -- switched to latex beamer for slides] -- fixed "time" missing in output bug.

--- src/sage/server/support.py
+++ src/sage/server/support.py
@@ -143,7 +143,7 @@ def source_code(s, globs):
         z = inspect.getsourcelines(obj)[0]
     except (TypeError, IndexError):
         return "Source code for %s not available."%obj
-    return '\n'.join(z)
+    return ''.join(z)
 
 def tabulate(v, width=90):
     if len(v) == 0:
