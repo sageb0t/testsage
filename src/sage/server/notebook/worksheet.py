eb67a89710c13baf494f3a3748b8b6e4941e4e45
William Stein <wstein@ucsd.edu>
2006-06-30 16:50:22 +0000

    [project @ notebook -- support backslash for extending a line; also sort the list of vars.]

--- src/sage/server/notebook/worksheet.py
+++ src/sage/server/notebook/worksheet.py
@@ -541,7 +541,6 @@ class Worksheet:
         corresponding to attached files that have changed.
         """
         A = self.attached_files()
-        print A
         for F, tm in A.iteritems():
             try:
                 new_tm = os.path.getmtime(F)
@@ -722,6 +721,7 @@ class Worksheet:
         return True, t
 
     def preparse_input(self, input, C):
+        input = input.replace('\\\n','')
         introspect = C.introspect()
         if introspect:
             before_prompt, after_prompt = introspect
