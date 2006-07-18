b7b64bf57dc04b17eded93e762541ba558fce432
boothby@u.washington.edu <boothby@u.washington.edu>
2006-07-18 00:19:48 +0000

    [project @ made help window resizable] help_fix

--- src/sage/server/notebook/js.py
+++ src/sage/server/notebook/js.py
@@ -1408,7 +1408,7 @@ function print_window(worksheet) {
 /////////////////////////////////
 function show_help_window(worksheet) {
     help = window.open ("__help__.html","",
-    "menubar=1,scrollbars=1,width=700,height=600");
+    "menubar=1,scrollbars=1,width=800,height=600,resizable=1");
 }
 
 /********************* js math ***************************/
