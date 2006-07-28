466b151e65beef4e5d04cd1123428ca111ee45fc
William Stein <wstein@gmail.com>
2006-07-28 05:58:02 +0000

    [project @ rational reconstruction -- made ten times faster by using the pyrex version instead of an old python version that was accidently being used by default.]

--- src/sage/ext/rational.pyx
+++ src/sage/ext/rational.pyx
@@ -114,7 +114,6 @@ cdef class Rational(element.FieldElement):
 
     def __set_value(self, x, int base):
         cdef int n
-        cdef integer.Integer num, den
 
         if isinstance(x, Rational):
             set_from_Rational(self, x)
@@ -889,9 +888,6 @@ def pyrex_rational_reconstruction(integer.Integer a, integer.Integer m):
         x -- rings.rational.Rational
     """
     cdef Rational x
-    #if not isinstance(a, integer.Integer) or not isinstance(m, integer.Integer):
-    #    a = integer.Integer(a)
-    #    m = integer.Integer(m)
     x = Rational()
     mpq_rational_reconstruction(x.value, a.get_value()[0], m.get_value()[0])
     return x
