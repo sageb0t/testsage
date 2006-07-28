6f02d687cb6279cb542579d4fc6f77051aacd8ed
William Stein <wstein@gmail.com>
2006-07-28 07:37:43 +0000

    [project @ david harvey -- added bernoulli_mod_p function (fast implementation of buhler et al.)]

--- src/setup.py
+++ src/setup.py
@@ -150,6 +150,10 @@ ext_modules = [ \
                          'sage/ext/mpn_pylong.c', 'sage/ext/mpz_pylong.c'],
               libraries=['gmp']), \
 
+    Extension('sage.ext.bernoulli_mod_p',
+              sources = ['sage/ext/bernoulli_mod_p.pyx', 'sage/ext/arith.pyx'],
+              libraries=['ntl']), \
+
     Extension('sage.ext.intmod_pyx',
               sources = ['sage/ext/intmod_pyx.pyx']), \
 
