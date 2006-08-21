cef42aad99657a9ed257b61340a6432a36bde01e
wstein@gmail.com <wstein@gmail.com>
2006-08-21 06:24:09 +0000

    [project @ modular forms -- fix some bugs and not implemented errors.  in particular eisenstein series at levels had wrong constant terms (**serious**).]

--- src/sage/modular/modform/element.py
+++ src/sage/modular/modform/element.py
@@ -328,8 +328,8 @@ class EisensteinSeries(ModularFormElement):
         try:
             return self.__defining_params
         except AttributeError:
-            chi = self.__chi
-            psi = self.__psi
+            chi = self.__chi.primitive_character()
+            psi = self.__psi.primitive_character()
             k = self.weight()
             t = self.__t
             L = chi.conductor()
@@ -371,7 +371,7 @@ class EisensteinSeries(ModularFormElement):
             self.__character = self.__chi * (~self.__psi)
         return self.__character
 
-    def level_at_which_new(self):
+    def new_level(self):
         if self.__chi.is_trivial() and self.__psi.is_trivial() and self.weight() == 2:
             return factor(self.__t)[0][0]
         return self.L()*self.M()
