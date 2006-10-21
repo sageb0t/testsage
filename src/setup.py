a015ed834ac99041e5aa5a83e9e4bceb422258ab
William Stein <wstein@gmail.com>
2006-10-21 00:26:47 -0700

    Remove stdsage.c from being included twice.

--- src/setup.py
+++ src/setup.py
@@ -249,7 +249,7 @@ ext_modules = [ \
                          'sage/ext/arith.pyx']), \
 
     Extension('sage.structure.element',
-              sources = ['sage/structure/element.pyx', 'sage/ext/stdsage.c']), \
+              sources = ['sage/structure/element.pyx']), \
 
     Extension('sage.modules.module',
               sources = ['sage/modules/module.pyx']), \
@@ -261,7 +261,7 @@ ext_modules = [ \
               sources = ['sage/groups/group.pyx']), \
 
     Extension('sage.structure.sage_object',
-              sources = ['sage/structure/sage_object.pyx', 'sage/ext/stdsage.c']), \
+              sources = ['sage/structure/sage_object.pyx']), \
 
     Extension('sage.structure.gens',
               sources = ['sage/structure/gens.pyx']), \
