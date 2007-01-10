8a1925b45f683c963370b5e3758d8b9e15c21af2
Bobby Moretti <moretti@u.washington.edu>
2007-01-10 10:04:39 -0800

    Very basic calculus skeleton.

--- src/setup.py
+++ src/setup.py
@@ -660,7 +660,9 @@ setup(name        = 'sage',
 
                      'sage.ext',
 
-                     'sage.functions',
+                     'sage.calculus',
+
+                     #'sage.functions',
 
                      'sage.geometry',
 
