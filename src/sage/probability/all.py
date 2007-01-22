2b404cb881f638d931714d934758f0ed39d4b6c7
David Kohel <kohel@maths.usyd.edu.au>
2007-01-22 11:55:13 +1100

    Information theory and probability package

---
+++ src/sage/probability/all.py
@@ -0,0 +1,7 @@
+from random_variable import (
+     is_ProbabilitySpace,
+     is_DiscreteProbabilitySpace,
+     is_RandomVariable,
+     is_DiscreteRandomVariable,
+     DiscreteRandomVariable,
+     DiscreteProbabilitySpace )
