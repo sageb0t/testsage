9b4b1084c303a03d368dbe5115a84bd00043243b
William Stein <wstein@gmail.com>
2007-01-23 18:33:57 -0800

    base_extend was too permissive for polynomial

--- src/sage/rings/polynomial_element.py
+++ src/sage/rings/polynomial_element.py
@@ -768,8 +768,7 @@ class Polynomial(commutative_algebra_element.CommutativeAlgebraElement):
         """
         Return a copy of this polynomial but with coefficients in R.
         """
-        S = sage.rings.polynomial_ring.PolynomialRing(R,
-                                      names = self.parent().variable_name())
+        S = self.parent().base_extend(R)
         return S(self)
 
     def __copy__(self):
