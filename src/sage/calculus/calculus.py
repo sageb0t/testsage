80eb0c5084e37bb82084b16ac63a0006a3634f7a
Bobby Moretti <moretti@u.washington.edu>
2007-02-09 01:44:50 -0800

    Improved preparsing.

--- src/sage/calculus/calculus.py
+++ src/sage/calculus/calculus.py
@@ -42,7 +42,7 @@ class SymbolicExpressionRing_class(CommutativeRing):
             return SymbolicPolynomial(x)
         elif isinstance(x, Integer):
             return Constant_object(x)
-
+        else:
             return Symbolic_object(x)
 
     def _repr_(self):
