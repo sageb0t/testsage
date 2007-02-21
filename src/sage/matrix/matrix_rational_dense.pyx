0388dc409047a0c43908eb04dca1c537e5932133
Soroosh Yazdani <syazdani@gmail.com>
2007-02-20 23:39:02 -0800

    bug fix
        * Not freeing temporary variable
        * Calling mpq vs mpz

--- src/sage/matrix/matrix_rational_dense.pyx
+++ src/sage/matrix/matrix_rational_dense.pyx
@@ -383,7 +383,8 @@ cdef class Matrix_rational_dense(matrix_dense.Matrix_dense):
                 mpq_mul(y, w._entries[j], self._matrix[j][i])
                 mpq_add(x, x, y)
             mpq_set(ans._entries[i], x)
-        mpz_clear(x)
+        mpq_clear(x)
+        mpq_clear(y)
         return ans
 
     def __neg__(self):
