f27a95fae30a91d6d7d64645c1fc7130955d6a42
David Roe <roed@math.harvard.edu>
2007-03-14 09:36:25 -0700

    Working on debugging, added branches of p-adic log

--- src/sage/rings/padics/eisenstein_extension_generic.py
+++ src/sage/rings/padics/eisenstein_extension_generic.py
@@ -58,3 +58,6 @@ class EisensteinExtensionGeneric(pAdicGeneric, PolynomialQuotientRing_domain):
 
         #For our use of the shift1list in shifting, we want it over by 1, so we pop.
         self._shift1list.pop(0)
+
+    def _uniformizer_print(self):
+        return self.variable_name()
