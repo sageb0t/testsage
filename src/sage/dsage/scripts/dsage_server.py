b98726782680484202f7b37cd3d7cbec6b303be3
Yi Qiang <yqiang@gmail.com>
2007-04-08 14:51:49 -0700

    Switched print to log

--- src/sage/dsage/scripts/dsage_server.py
+++ src/sage/dsage/scripts/dsage_server.py
@@ -152,8 +152,8 @@ def main():
                     SSL_CERT)
         while True:
             if attempts > 50:
-                print err_msg
-                print 'Last attempted port: %s' % (CLIENT_PORT)
+                log.err(err_msg)
+                log.err('Last attempted port: %s' % (CLIENT_PORT))
             try:
                 reactor.listenSSL(CLIENT_PORT,
                                   client_factory,
@@ -165,8 +165,8 @@ def main():
     else:
         while True:
             if attempts > 50:
-                print err_msg
-                print 'Last attempted ports: %s' % (CLIENT_PORT)
+                log.err(err_msg)
+                log.err('Last attempted port: %s' % (CLIENT_PORT))
             try:
                 reactor.listenTCP(CLIENT_PORT, client_factory)
                 break
