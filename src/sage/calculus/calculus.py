ea3088e398ad970eba748d6ce27ffbcf24ffebba
William Stein <wstein@gmail.com>
2007-04-23 10:45:08 -0700

    fix some texing of calculus expressions.

--- src/sage/calculus/calculus.py
+++ src/sage/calculus/calculus.py
@@ -2474,6 +2474,8 @@ common_varnames = ['alpha',
 def tex_varify(a):
     if a in common_varnames:
         return "\\" + a
+    elif len(a) == 1:
+        return a
     else:
         return '\\mbox{%s}'%a
 
