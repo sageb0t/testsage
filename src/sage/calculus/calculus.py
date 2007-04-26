4543310e776897746980e501584d2796b57d62e2
William Stein <wstein@gmail.com>
2007-04-25 20:35:53 -0700

    fix in setup.py so works on windows (and some commented out code in calculus.py)

--- src/sage/calculus/calculus.py
+++ src/sage/calculus/calculus.py
@@ -1914,6 +1914,10 @@ class Symbolic_object(SymbolicExpression):
         SymbolicExpression.__init__(self)
         self._obj = obj
 
+    #def derivative(self, *args):
+        # TODO: remove
+    #    return self.parent().zero_element()
+
     def obj(self):
         """
         EXAMPLES:
