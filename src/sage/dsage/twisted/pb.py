73ee556ac95d712670c0c86b4da18d17933d9b27
Yi Qiang <yqiang@gmail.com>
2007-05-11 13:22:49 -0700

    Cleaned up the code.

--- src/sage/dsage/twisted/pb.py
+++ src/sage/dsage/twisted/pb.py
@@ -238,12 +238,6 @@ class AnonymousMonitorPerspective(DefaultPerspective):
 
         return self.DSageServer.job_done(job_id, output, result, completed)
 
-    def perspective_submit_host_info(self, hostinfo):
-        if not isinstance(hostinfo, dict):
-            raise BadTypeError()
-
-        return self.DSageServer.submit_host_info(hostinfo)
-
 class MonitorPerspective(AnonymousMonitorPerspective):
     """
     Defines the perspective of an authenticated worker to the server.
@@ -364,11 +358,6 @@ class UserPerspective(DefaultPerspective):
 
     def perspective_get_worker_count(self):
         return self.DSageServer.get_worker_count()
-
-    def perspective_submit_host_info(self, hostinfo):
-        if not isinstance(hostinfo, dict):
-            raise BadTypeError()
-        return self.DSageServer.submit_host_info(hostinfo)
 
     def perspective_get_killed_jobs_list(self):
         return self.DSageServer.get_killed_jobs_list()
