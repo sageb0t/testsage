91744eb1cf186b908db84ec192a9188ac33d38b3
William Stein <wstein@gmail.com>
2007-05-25 19:43:52 -0700

    Fix trac #380: bug when defining a relative extension

--- src/sage/rings/number_field/number_field.py
+++ src/sage/rings/number_field/number_field.py
@@ -529,7 +529,7 @@ class NumberField_generic(field.Field):
             name = name[0]
         if name is None:
             raise TypeError, "the variable name must be specified."
-        return NumberField_extension(self, poly, repr(name))
+        return NumberField_extension(self, poly, str(name))
 
     def factor_integer(self, n):
         r"""
