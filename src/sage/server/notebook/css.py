a1601fc4f0b114cd02c3eac0351efd775b8d6582
William Stein <wstein@gmail.com>
2007-05-30 16:25:45 -0700

    notebook restyling.

--- src/sage/server/notebook/css.py
+++ src/sage/server/notebook/css.py
@@ -585,7 +585,7 @@ button.delete_worksheet_menu {
 span.pane div.worksheet_list {
    font-size:12px;
    top:0ex;
-   height:25ex;
+   height:45ex;
    border:2px solid <color2>;
    overflow:auto;
    width: 180px;
@@ -678,7 +678,7 @@ span.pane a.worksheet_current_computing {
  }
 
 span.pane a.worksheet_other {
-   font-size:12px;
+   font-size:14px;
    padding-left:1ex;
    border-top:1px solid <color2>;
    background-color:white;
