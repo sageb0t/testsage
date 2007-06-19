ad93d29b1d1838716f7e778e04c4c87471e535e5
William Stein <wstein@gmail.com>
2007-06-19 07:55:34 -0700

    twisted notebook -- idle timeouts; worksheet conf

--- src/sage/server/notebook/user_conf.py
+++ src/sage/server/notebook/user_conf.py
@@ -3,5 +3,5 @@ import conf
 defaults = {}
 
 class UserConfiguration(conf.Configuration):
-    def __init__(self):
-        conf.Configuration.__init__(self, defaults)
+    def defaults(self):
+        return defaults
