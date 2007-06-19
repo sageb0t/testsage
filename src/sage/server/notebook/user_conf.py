50cb08232178b0649eef2c5f91cdc516e519c799
William Stein <wstein@gmail.com>
2007-06-18 18:07:30 -0700

    Added user and server conf objects; cleaned up avatars.py; got rid of passwords.txt.

--- src/sage/server/notebook/user_conf.py
+++ src/sage/server/notebook/user_conf.py
@@ -1,2 +1,7 @@
-class UserConfiguration:
-    pass
+import conf
+
+defaults = {}
+
+class UserConfiguration(conf.Configuration):
+    def __init__(self):
+        conf.Configuration.__init__(self, defaults)
