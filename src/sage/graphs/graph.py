c47e48e5462e6b3b8e06235fdd47c5be81b491c1
Robert L Miller <rlm@robertlmiller.com>
2007-07-22 09:44:59 -0700

    ...

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -3621,7 +3621,6 @@ class Graph(GenericGraph):
             faces = len(t)
             if faces > max_faces:
                 max_faces = faces
-        print max_faces
         return (2-verts+edges-max_faces)/2
 
     def interior_paths(self, start, end):
