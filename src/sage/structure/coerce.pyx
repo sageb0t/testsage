7bc9b0995fa97b72524fc58b838a017d552755c7
Robert Bradshaw <robertwb@math.washington.edu>
2007-09-11 10:52:07 -0700

    fix doctests, re-arrange action finding order

--- src/sage/structure/coerce.pyx
+++ src/sage/structure/coerce.pyx
@@ -437,15 +437,19 @@ Original elements %r (parent %s) and %r (parent %s) and morphisms
             sageR = py_scalar_parent(R)
             if sageR is not None:
                 action = self.discover_action_c(sageR, S, op)
-                if action and not PY_TYPE_CHECK(action, IntegerMulAction):
-                    return PyScalarAction(action)
+                if action:
+                    if not PY_TYPE_CHECK(action, IntegerMulAction):
+                        action = PyScalarAction(action)
+                    return action
 
         if PY_TYPE(S) == <void *>type:
             sageS = py_scalar_parent(S)
             if sageS is not None:
                 action = self.discover_action_c(R, sageS, op)
-                if action and not PY_TYPE_CHECK(action, IntegerMulAction):
-                    return PyScalarAction(action)
+                if action:
+                    if not PY_TYPE_CHECK(action, IntegerMulAction):
+                        action = PyScalarAction(action)
+                    return action
 
         if op.__name__[0] == 'i':
             try:
@@ -859,6 +863,9 @@ cdef class PyScalarAction(Action):
             b = self.G(b)
             return self._action._call_c(a,b)
 
+    def __inverse__(self):
+        return PyScalarAction(~self._action)
+
 cdef class IntegerMulAction(Action):
 
     def __init__(self, ZZ, M, is_left):
@@ -872,7 +879,6 @@ cdef class IntegerMulAction(Action):
         # Override _call_c because signature of _call_c_impl requires elements
         if not self._is_left:
             a, nn = nn, a
-        print type(nn)
         if not PyInt_CheckExact(nn):
             nn = PyNumber_Int(nn)
             if not PyInt_CheckExact(nn):
