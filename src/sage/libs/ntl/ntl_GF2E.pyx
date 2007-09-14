84d8797ad8692e1ef2ea3f39398914f26efc5d50
Joel B. Mohler <jbm5@lehigh.edu>
2007-09-14 09:02:25 -0400

    Added a bunch of more doc-tests to NTL wrappers

--- src/sage/libs/ntl/ntl_GF2E.pyx
+++ src/sage/libs/ntl/ntl_GF2E.pyx
@@ -285,6 +285,7 @@ cdef class ntl_GF2E(ntl_GF2X):
         cdef ntl_GF2E r = ntl_GF2E()
         _sig_on
         r.gf2e_x = self.gf2e_x
+        r.gf2x_x = self.gf2x_x
         _sig_off
         return r
 
@@ -295,6 +296,7 @@ cdef class ntl_GF2E(ntl_GF2X):
         cdef ntl_GF2E r = ntl_GF2E()
         _sig_on
         r.gf2e_x = self.gf2e_x
+        r.gf2x_x = self.gf2x_x
         _sig_off
         return r
 
