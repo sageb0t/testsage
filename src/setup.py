83456f28b85130c84f14e571c8cc418dd185cc7f
Mike Hansen <mhansen@gmail.com>
2007-09-20 12:04:19 -0700

    Changes to setup.py to accomodate symmetrica.

--- src/setup.py
+++ src/setup.py
@@ -308,6 +308,11 @@ dwt = Extension('sage.gsl.dwt',['sage/gsl/dwt.pyx'],
 
 sagex_ds = Extension('sage.misc.sagex_ds', ['sage/misc/sagex_ds.pyx'])
 
+symmetrica = Extension('sage.libs.symmetrica.symmetrica',
+                       sources = ["sage/libs/symmetrica/%s"%s for s in \
+                                  ["symmetrica.pyx"]],
+                       libraries = ["symmetrica"])
+
 #####################################################
 
 ext_modules = [ \
@@ -389,6 +394,8 @@ ext_modules = [ \
     complex_number,
 
     sagex_ds,
+
+    symmetrica,
 
     Extension('sage.media.channels',
               sources = ['sage/media/channels.pyx']), \
@@ -908,6 +915,7 @@ setup(name        = 'sage',
                      'sage.libs.flint',
                      'sage.libs.pari',
                      'sage.libs.singular',
+                     'sage.libs.symmetrica',
 
                      'sage.logic',
 
