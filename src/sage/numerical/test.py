992de3b976072950f4f9e241f8209cd79187da1e
kantor@cpe-76-180-115-46.buffalo.res.rr.com <kantor@cpe-76-180-115-46.buffalo.res.rr.com>
2007-09-20 10:53:26 -0700

    added doctests for scipy,cvxopt

---
+++ src/sage/numerical/test.py
@@ -0,0 +1,12 @@
+"""
+Doctests for cvxopt, scipy
+
+sage: from cvxopt.base import *
+sage: from cvxopt import umfpack
+sage: from scipy import optimize
+sage: from scipy import special
+sage: from scipy import integrate
+sage: from scipy import linsolve
+sage: from scipy import interpolate
+sage: from scipy import sparse
+"""
