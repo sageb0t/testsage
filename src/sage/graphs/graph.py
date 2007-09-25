870309c229a39fb92e063c4405e84fdefed8beaa
Jason Grout <jason-sage@creativetrax.com>
2007-09-25 10:24:41 -0500

    graphs: changes to docs of clique functions.  cliques_max_clique_graph now has a name option and automatically tells networkx to construct an XGraph for performance reasons.

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -969,8 +969,8 @@ class GenericGraph(SageObject):
 
     def cliques(self):
         """
-        Returns the list of maximal cliques of which each are members
-        of the clique.
+        Returns the list of maximal cliques.  Each maximal clique is
+        represented by a list of vertices.
 
         Currently only implemented for undirected graphs.  Use to_undirected
         to convert a digraph to an undirected graph.  (See examples below).
@@ -1005,7 +1005,7 @@ class GenericGraph(SageObject):
             import networkx.cliques
             return networkx.cliques.find_cliques(self._nxg)
 
-    def cliques_get_max_clique_graph(self, **kwds):
+    def cliques_get_max_clique_graph(self, name=''):
         """
         Returns a graph constructed with maximal cliques as vertices,
         and edges between maximal cliques with common members in
@@ -1013,6 +1013,9 @@ class GenericGraph(SageObject):
 
         Currently only implemented for undirected graphs.  Use to_undirected
         to convert a digraph to an undirected graph.  (See examples below).
+
+        INPUT:
+           name -- The name of the new graph.
 
         EXAMPLES:
             sage: (graphs.ChvatalGraph()).cliques_get_max_clique_graph()
@@ -1034,7 +1037,7 @@ class GenericGraph(SageObject):
             raise TypeError('Function defined for undirected graphs only.  See documentation.')
         else:
             import networkx.cliques
-            return Graph(networkx.cliques.make_max_clique_graph(self._nxg, **kwds))
+            return Graph(networkx.cliques.make_max_clique_graph(self._nxg, name=name, create_using=networkx.xgraph.XGraph()))
 
     # Add fpos (below) when Bipartite class is wrapped.
     def cliques_get_clique_bipartite(self, **kwds):
