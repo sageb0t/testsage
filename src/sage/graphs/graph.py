22d96694c7ad3278187bdc980fac551447fa6b0c
William Stein <wstein@gmail.com>
2007-10-13 21:16:15 -0700

    Better fix for #878.

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -2098,8 +2098,8 @@ class GenericGraph(SageObject):
             [1 1 0 0 1 1]
             [1 0 1 1 0 1]
             [0 1 1 1 1 0]
-            sage: g=DiGraph([[1..4],lambda i,j: i<j])
-            sage: h=g.line_graph()
+            sage: g = DiGraph([[1..4],lambda i,j: i<j])
+            sage: h = g.line_graph()
             sage: h.vertices()
             [(1, 2, None),
             (1, 3, None),
@@ -2108,11 +2108,10 @@ class GenericGraph(SageObject):
             (2, 4, None),
             (3, 4, None)]
             sage: h.edges()
-            [((2, 3, None), (3, 4, None), None),
-            ((1, 2, None), (2, 4, None), None),
-            ((1, 2, None), (2, 3, None), None),
-            ((1, 3, None), (3, 4, None), None)]
-
+            [((1, 2, None), (2, 3, None), None),
+             ((1, 2, None), (2, 4, None), None),
+             ((1, 3, None), (3, 4, None), None),
+             ((2, 3, None), (3, 4, None), None)]
         """
         if self.is_directed():
             G=DiGraph()
@@ -3309,13 +3308,19 @@ class Graph(GenericGraph):
         """
         self._nxg.delete_multiedge(u, v)
 
-    def edges(self, labels=True):
+    def edges(self, labels=True, sort=True):
         """
-        Return a list of edges. Each edge is a triple (u,v,l) where u and v are
-        vertices and l is a label.
+        Return a list of edges. Each edge is a triple (u,v,l) where u
+        and v are vertices and l is a label.
 
         INPUT:
-        labels -- if False, each edge is a tuple (u,v) of vertices.
+            labels -- (bool; default: True) if False, each edge is a
+                      tuple (u,v) of vertices.
+            sort -- (bool; default: True) if True, ensure that the list
+                    of edges is sorted.
+
+        OUTPUT:
+            A list of tuples.  It is safe to change the returned list.
 
         EXAMPLES:
             sage: graphs.DodecahedralGraph().edges()
@@ -3323,16 +3328,14 @@ class Graph(GenericGraph):
 
             sage: graphs.DodecahedralGraph().edges(labels=False)
             [(0, 1), (0, 10), (0, 19), (1, 8), (1, 2), (2, 3), (2, 6), (3, 19), (3, 4), (4, 17), (4, 5), (5, 6), (5, 15), (6, 7), (7, 8), (7, 14), (8, 9), (9, 10), (9, 13), (10, 11), (11, 12), (11, 18), (12, 16), (12, 13), (13, 14), (14, 15), (15, 16), (16, 17), (17, 18), (18, 19)]
-
         """
         L = self._nxg.edges()
+        if sort:
+            L.sort()
         if labels:
             return L
         else:
-            K = []
-            for u,v,l in L:
-                K.append((u,v))
-            return K
+            return [(u,v) for u,v,_ in L]
 
     def edge_boundary(self, vertices1, vertices2=None, labels=True):
         """
@@ -5281,13 +5284,19 @@ class DiGraph(GenericGraph):
         """
         self._nxg.delete_multiedge(u, v)
 
-    def edges(self, labels=True):
+    def edges(self, labels=True, sort=True):
         """
         Return a list of edges. Each edge is a triple (u,v,l) where the edge is
         from u to v, with label l.
 
         INPUT:
-        labels -- if False, each edge is a tuple (u,v) of vertices.
+            labels -- (bool; default: True) if False, each edge is a
+                      tuple (u,v) of vertices.
+            sort -- (bool; default: True) if True, ensure that the list
+                    of edges is sorted.
+
+        OUTPUT:
+            A list of tuples.  It is safe to change the returned list.
 
         EXAMPLES:
             sage: D = graphs.DodecahedralGraph().to_directed()
@@ -5298,13 +5307,12 @@ class DiGraph(GenericGraph):
 
         """
         L = self._nxg.edges()
+        if sort:
+            L.sort()
         if labels:
             return L
         else:
-            K = []
-            for u,v,l in L:
-                K.append((u,v))
-            return K
+            return [(u,v) for u,v,_ in L]
 
     def edge_boundary(self, vertices1, vertices2=None, labels=True):
         """
