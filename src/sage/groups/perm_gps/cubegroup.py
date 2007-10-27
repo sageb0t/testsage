2e1634ad7f6923b5d69eb0211a63f06df9729c46
Carl Witty <cwitty@newtonlabs.com>
2007-10-27 13:21:24 -0700

    Minor doctest fixes.

--- src/sage/groups/perm_gps/cubegroup.py
+++ src/sage/groups/perm_gps/cubegroup.py
@@ -1179,7 +1179,7 @@ class RubiksCube(SageObject):
         """
         sage: C = RubiksCube().move("R*U")
         sage: C.plot3d()
-        <class 'sage.plot.plot3d.base.TransformGroup'>
+        <class 'base.TransformGroup'>
         sage: C.plot()
         Graphics object consisting of 55 graphics primitives
         """
