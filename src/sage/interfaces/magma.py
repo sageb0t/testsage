03798e397e86354a195fc0d72f8388f8dfbe54cb
Martin Albrecht <malb@informatik.uni-bremen.de>
2007-10-30 16:36:36 +0000

    allow boolean parameters to be passed to magma functions, this fixes #845

--- src/sage/interfaces/magma.py
+++ src/sage/interfaces/magma.py
@@ -309,6 +309,8 @@ class Magma(Expect):
             Variables: x, y, z
         """
         if gens is None:
+            if isinstance(x, bool):
+                return Expect.__call__(self, str(x).lower())
             return Expect.__call__(self, x)
         return self.objgens(x, gens)
 
@@ -391,7 +393,7 @@ class Magma(Expect):
         if len(params) == 0:
             par = ''
         else:
-            par = ' : ' + ','.join(['%s:=%s'%(a,b) for a,b in params.items()])
+            par = ' : ' + ','.join(['%s:=%s'%(a,self(b)) for a,b in params.items()])
 
         fun = "%s(%s%s)"%(function, ",".join([s.name() for s in args]), par)
         if nvals <= 0:
