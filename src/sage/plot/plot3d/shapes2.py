cf82c0c48308ed568447924d6f2117f3f8cb6dcc
William Stein <wstein@gmail.com>
2007-12-31 19:57:47 -0700

    3d graphics -- parametric surfaces.

--- src/sage/plot/plot3d/shapes2.py
+++ src/sage/plot/plot3d/shapes2.py
@@ -1,6 +1,8 @@
 import shapes
 
 def line3d(points, coerce=True, color="lightblue", **kwds):
+    if len(points) < 2:
+        raise ValueError, "there must be at least 2 points"
     v = []
     for i in range(len(points) - 1):
         v.append(shapes.LineSegment(points[i], points[i+1], color=color, **kwds))
