e830a0bc46d4b56c0e7fa624ab8a68ca33482a56
dmharvey@math.harvard.edu <dmharvey@math.harvard.edu>
2008-01-08 17:31:05 -0500

    wrote padic_sigma_truncated()

--- src/sage/schemes/elliptic_curves/ell_rational_field.py
+++ src/sage/schemes/elliptic_curves/ell_rational_field.py
@@ -2834,6 +2834,7 @@ class EllipticCurve_rational_field(EllipticCurve_number_field):
     padic_height = padics.padic_height
 
     padic_sigma = padics.padic_sigma
+    padic_sigma_truncated = padics.padic_sigma_truncated
 
     padic_E2 = padics.padic_E2
 
