2b5fded528861bf4b75541a9ee23b36d52deca41
mabshoff@sage.math.washington.edu <mabshoff@sage.math.washington.edu>
2008-03-12 14:17:23 -0700

    Fix doctest failure due to the patch from #2421 (#2503)

--- src/sage/misc/functional.py
+++ src/sage/misc/functional.py
@@ -842,7 +842,7 @@ def round(x, ndigits=0):
         3.0
         sage: b = 5.4999999999999999
         sage: round(b)
-        5.0000000000000000
+        5
 
     IMPLEMENTATION: If ndigits is specified, it calls Python's builtin round function,
     and converts the result to a real double field element. Otherwise, it tries the
