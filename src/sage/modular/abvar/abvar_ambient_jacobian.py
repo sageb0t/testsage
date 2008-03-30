b6e75a30336775fd1a417720469a34c62964c596
William Stein <wstein@gmail.com>
2008-03-30 00:19:45 -0700

    modabvar -- a ton of optimization; probably broke lots of things (?)

--- src/sage/modular/abvar/abvar_ambient_jacobian.py
+++ src/sage/modular/abvar/abvar_ambient_jacobian.py
@@ -9,8 +9,11 @@ TESTS:
 """
 
 import weakref
+from sage.structure.sequence import Sequence
 
-from abvar             import ModularAbelianVariety_modsym_abstract, ModularAbelianVariety
+from abvar             import (ModularAbelianVariety_modsym_abstract, ModularAbelianVariety,
+                               simple_factorization_of_modsym_space,
+                               ModularAbelianVariety_modsym)
 from sage.rings.all    import QQ
 from sage.modular.dims import dimension_cusp_forms
 
@@ -258,3 +261,25 @@ class ModAbVar_ambient_jacobian_class(ModularAbelianVariety_modsym_abstract):
             d = dimension_cusp_forms(self.group(), k=2)
             self._dimension = d
             return d
+
+    def decomposition(self, simple=True, bound=None):
+        try:
+            return self.__decomposition[simple]
+        except KeyError:
+            pass
+        except AttributeError:
+            pass
+
+        M = self.modular_symbols().ambient_module()
+        group = M.group()
+        factors = simple_factorization_of_modsym_space(M)
+
+        D = []
+        for newform_level, isogeny_number, number, modsym in factors:
+            A = ModularAbelianVariety_modsym(modsym, (newform_level, group), is_simple=True,
+                               isogeny_number=isogeny_number, number=number, check=False)
+            D.append(A)
+
+        D.sort()
+        self.__decomposition = Sequence(D, immutable=True, cr=True, universe=self.category())
+        return D
