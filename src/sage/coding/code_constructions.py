sageb0t/testsage

commit 6c8292bbb87f1688f8d53836f7c840258086a6f8

Fix documentation for Sage 3.0.alpha2 (#2862)

src/sage/coding/code_constructions.py
@@ -141,7 +141,7 @@ def cyclotomic_cosets(q, n, t = None):
     the q-cyclotomic coset mod n containing t)
 
     Let q, n be relatively print positive integers and let
-    A = q^{ZZ}. The group A acts on ZZ/nZZ by multiplication.
+    $A = q^{ZZ}$. The group A acts on ZZ/nZZ by multiplication.
     The orbits of this action are "cyclotomic cosets",
     or more precisely "q-cyclotomic cosets mod n". Sometimes the
     smallest element of the coset is called the "coset leader".
@@ -221,7 +221,7 @@ def is_a_splitting(S1,S2,n):
     (if a = True) or 0 (if a = False)
 
     Splittings are useful for computing idempotents in the
-    quotient ring Q = GF(q)[x]/(x^n-1). For
+    quotient ring $Q = GF(q)[x]/(x^n-1)$. For
 
     EXAMPLES:
         sage: from sage.coding.code_constructions import is_a_splitting
@@ -576,7 +576,7 @@ def CyclicCodeFromGeneratingPolynomial(n,g,ignore=True):
         [0 0 1 1]
 
     On the other hand, CyclicCodeFromPolynomial(4,x) will produce
-    a ValueError including a traceback error message: "x must divide x^4 - 1".
+    a ValueError including a traceback error message: "$x$ must divide $x^4 - 1$".
     You will also get a ValueError if you type
         sage: P.<x> = PolynomialRing(GF(4,"a"),"x")
         sage: g = x^2+1
@@ -589,7 +589,7 @@ def CyclicCodeFromGeneratingPolynomial(n,g,ignore=True):
         Linear code of length 5, dimension 4 over Finite Field of size 3
 
     followed by C = CyclicCodeFromGeneratingPolynomial(5,g,False), with
-    a traceback message including "x^2 + 2 must divide x^5 - 1".
+    a traceback message including "$x^2 + 2$ must divide $x^5 - 1$".
 
     """
     P = g.parent()
@@ -853,7 +853,7 @@ def HammingCode(r,F):
         sage: C.minimum_distance()
         3
         sage: C = HammingCode(3,GF(4,'a')); C
-        Linear code of length 21, dimension 18 over Finite Field in a of size 2^2
+        Linear code of length 21, dimension 18 over Finite Field in a of size $2^2$
 
     """
     q = F.order()
@@ -872,7 +872,7 @@ def LinearCodeFromCheckMatrix(H):
     matrix G and check matrix H. We have the following short
     exact sequence
 
-    \[
+    \begin{equation}
     0 \rightarrow
     {\mathbf{F}}^k \stackrel{G}{\rightarrow}
     {\mathbf{F}}^n \stackrel{H}{\rightarrow}

src/sage/coding/sd_codes.py
@@ -36,7 +36,7 @@ case-by-case list of entries, each represented by a Python dictionary.
          C = self_dual_codes_binary(n); m = len(C["%s"%n].keys())
          for i in range(m):
              C0 = C["%s"%n]["%s"%i]["code"]
-             #print n,i,C0
+             \#print n,i,C0
              if C0.minimum_distance()>2:
                  f = R(C0.sd_zeta_polynomial())
                  print n,i,[z[0].abs() for z in f.roots()]
@@ -139,11 +139,11 @@ def self_dual_codes_binary(n):
     The number of inequiv "diagonal" sd binary codes in the database of length n
     is ("diagonal" is defined by the conjecture above) is the same as
     the restricted partition number of n, where only integers from
-    the set {1,4,6,8,...} are allowed. This is the coeff of x^n in the
-    series expansion $(1-x)^{-1}\prod_{2^\infty (1-x^{2j})^{-1}$. Typing
+    the set {1,4,6,8,...} are allowed. This is the coeff of $x^n$ in the
+    series expansion $(1-x)^{-1}\prod_{2^\infty (1-x^{2j})^{-1}}$. Typing
     the command
-    f = (1-x)^(-1)*prod([(1-x^(2*j))^(-1) for j in range(2,18)])
-    into SAGE, we obtain for the coeffs of x^4, x^6, ...
+    f = (1-x)\^(-1)*prod([(1-x\^(2*j))\^(-1) for j in range(2,18)])
+    into SAGE, we obtain for the coeffs of $x^4$, $x^6$, ...
     [1, 1, 2, 2, 3, 3, 5, 5, 7, 7, 11, 11, 15, 15, 22, 22, 30, 30,
      42, 42, 56, 56, 77, 77, 101, 101, 135, 135, 176, 176, 231]
     These numbers grow too slowly to account for all the sd codes (see

src/sage/modular/abvar/abvar.py
@@ -2748,7 +2748,7 @@ class ModularAbelianVariety_abstract(ParentWithBase):
     def _isogeny_to_product_of_simples(self):
         r"""
         Given an abelian variety $A$, return an isogeny
-        $\phi: A \rightarrow B_1 \times \cdots \times \B_n$,
+        $\phi: A \rightarrow B_1 \times \cdots \times B_n$,
         where each $B_i$ is simple. Note that this isogeny is
         not unique.
 
@@ -2799,7 +2799,7 @@ class ModularAbelianVariety_abstract(ParentWithBase):
     def _isogeny_to_product_of_powers(self):
         r"""
         Given an abelian variety $A$, return an isogeny
-        $\phi: A \rightarrow B_1 \times \cdots \times \B_n$, where
+        $\phi: A \rightarrow B_1 \times \cdots \times B_n$, where
         each $B_i$ is a power of a simple abelian variety.  These
         factors will be exactly those returned by
         self.decomposition(simple=False).Note that this isogeny is not

src/sage/modular/abvar/morphism.py
@@ -804,7 +804,7 @@ class HeckeOperator(Morphism):
     def matrix(self):
         """
         Return the matrix of self acting on the homology $H_1(A, ZZ)$
-        of this abelian variety with coefficients in $\mathbb{Z}$.
+        of this abelian variety with coefficients in $\ZZ$.
 
         EXAMPLES:
             sage: J0(47).hecke_operator(3).matrix()

src/sage/rings/complex_number.pyx
@@ -224,7 +224,7 @@ cdef class ComplexNumber(sage.structure.element.FieldElement):
     def _set_multiplicative_order(self, n):
         r"""
         Function for setting the \code{ComplexNumber} class attribute
-        \code{multiplicative_order) of self.
+        \code{multiplicative_order} of self.
 
         INPUTS:
             n -- an integer which will define the multiplicative order of self
@@ -363,7 +363,7 @@ cdef class ComplexNumber(sage.structure.element.FieldElement):
         r"""
         Returns the norm of self.
 
-            norm(a + bi) = a^2 + b^2
+            $norm(a + bi) = a^2 + b^2$
 
         EXAMPLES:
         This indeed acts as the square function when the imaginary
@@ -583,7 +583,7 @@ cdef class ComplexNumber(sage.structure.element.FieldElement):
         r"""
         Method for computing the absolute value or modulus of self
 
-            |a + bi| = sqrt(a^2 + b^2)
+            $|a + bi| = sqrt(a^2 + b^2)$
 
         EXAMPLES:
         Note that the absolute value of a complex number with imaginary
@@ -1041,13 +1041,9 @@ cdef class ComplexNumber(sage.structure.element.FieldElement):
         Returns the complex dilogarithm of self. The complex dilogarithm, or
         Spence's function, is defined by
 
-                                 log |1 - zeta|
-            Li_2(z) = - \int_0^z -------------- d(zeta)
-                                      zeta
+            $Li_2(z) = - \int_0^z \frac{\log|1-\zeta|}{\zeta} d(\zeta)$
 
-                                         z^k
-                    = \sum_{k=1}^\infty -----
-                                          k
+                    $= \sum_{k=1}^\infty \frac{z^k}{k}$
 
         Note that the series definition can only be used for $|z| < 1$
         EXAMPLES:

src/sage/rings/polynomial/pbori.pyx
@@ -925,7 +925,7 @@ cdef class BooleanPolynomialRing(MPolynomialRing_generic):
         Return a newly created \Singular quotient ring matching this
         boolean polynomial ring.
 
-        NOTE \& TODO: This method does not only return a string but
+        NOTE & TODO: This method does not only return a string but
         actually calls \Singular.
 
         EXAMPLE:
@@ -1041,7 +1041,7 @@ cdef class BooleanPolynomialRing(MPolynomialRing_generic):
 
         WARNING: Do not use this method. It is provided for
         compatibility reasons with \PolyBoRi but parents are supposed
-        to be immutable in \Sage.
+        to be immutable in \sage.
         """
         if order < 0 or order > 4:
             raise ValueError, "order value %s is not supported"%(order)
@@ -1068,7 +1068,7 @@ cdef class BooleanPolynomialRing(MPolynomialRing_generic):
 
         WARNING: Do not use this method. It is provided for
         compatibility reasons with \PolyBoRi but parents are supposed
-        to be immutable in \Sage.
+        to be immutable in \sage.
         """
         self._pbring.activate()
         pb_set_variable_name(i, s)
@@ -1077,9 +1077,9 @@ cdef class BooleanPolynomialRing(MPolynomialRing_generic):
         self._names = tuple(t)
 
 def get_var_mapping(ring, other):
-    """
+    r"""
     Return a variable mapping between variables of \var{other} and
-    \var{ring]. When other is a parent object, the mapping defines
+    \var{ring}. When other is a parent object, the mapping defines
     images for all variables of other. If it is an element, only
     variables occuring in other are mapped.
 

src/sage/schemes/elliptic_curves/ell_finite_field.py
@@ -271,7 +271,7 @@ class EllipticCurve_finite_field(EllipticCurve_field, HyperellipticCurve_finite_
     random_point = random_element
 
     def trace_of_frobenius(self):
-        """
+        r"""
         Return the trace of Frobenius acting on this elliptic curve.
 
         NOTE:
@@ -285,7 +285,7 @@ class EllipticCurve_finite_field(EllipticCurve_field, HyperellipticCurve_finite_
             sage: E.trace_of_frobenius()
             802
 
-        The following shows that the issue from trac #2849 is fixed:
+        The following shows that the issue from trac \#2849 is fixed:
             sage: E=EllipticCurve(GF(3^5,'a'),[-1,-1])
             sage: E.trace_of_frobenius()
             -27

commit 33354880dd0a46463598c77fd05a20f0ae0eab68

Fix a small doctest failure introduced by #2862

src/sage/coding/code_constructions.py
@@ -853,7 +853,7 @@ def HammingCode(r,F):
         sage: C.minimum_distance()
         3
         sage: C = HammingCode(3,GF(4,'a')); C
-        Linear code of length 21, dimension 18 over Finite Field in a of size $2^2$
+        Linear code of length 21, dimension 18 over Finite Field in a of size 2^2
 
     """
     q = F.order()