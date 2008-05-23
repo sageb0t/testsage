0c924e4bc20f0b3d524a5c657f322a83afdb9712
mabshoff@sage.math.washington.edu <mabshoff@sage.math.washington.edu>
2008-05-23 08:47:40 -0700

    Fix outstanding docstring build issues (fixes #3280)

--- src/sage/rings/number_field/number_field_element.pyx
+++ src/sage/rings/number_field/number_field_element.pyx
@@ -1919,7 +1919,7 @@ cdef class NumberFieldElement_relative(NumberFieldElement):
         return repr(R(self.list()))
 
     def _latex_(self):
-        """
+        r"""
         Returns the latex representation for this element.
 
         EXAMPLES:
