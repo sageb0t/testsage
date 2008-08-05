61f94dae71aa718d3fd6b18f52ce0027e555581a
Timothy Clemans <timothy.clemans@gmail.com>
2008-08-05 11:43:04 -0700

    #3776 removed render from AdminToplevel so Notebook not broken for admins

--- src/sage/server/notebook/twist.py
+++ src/sage/server/notebook/twist.py
@@ -2197,12 +2197,6 @@ class AdminToplevel(UserToplevel):
     userchild_home = WorksheetsAdmin
     userchild_conf = NotebookConf
 
-    def render(self, ctx):
-        s = render_worksheet_list(ctx.args, pub=False, username=self.username)
-        return http.Response(responsecode.OK,
-                             {'set-cookie':set_cookie(self.cookie)},
-                             stream=s)
-
 def set_cookie(cookie):
     return [http_headers.Cookie(SID_COOKIE, cookie)]
 
