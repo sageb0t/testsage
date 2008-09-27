ebdd31c0fda3e589d47d8c60228c7cf31158d170
David Joyner <wdjoyner@gmail.com>
2008-09-26 20:23:57 -0400

    Forgot the block_design test- double arrgghh

--- src/sage/combinat/designs/block_design.py
+++ src/sage/combinat/designs/block_design.py
@@ -244,7 +244,7 @@ def HadamardDesign(n):
     # A is the incidence matrix of the block design
     return IncidenceStructureFromMatrix(A,name="HadamardDesign")
 
-def BlockDesign(max_pt, blks, name=None):
+def BlockDesign(max_pt, blks, name=None, test=True):
     """
     Returns an instance of the IncidenceStructure class. Requires each
     B in blks to be contained in range(max_pt). Does not test if
@@ -259,8 +259,11 @@ def BlockDesign(max_pt, blks, name=None):
     if nm == None:
         nm = "BlockDesign"
     BD = IncidenceStructure( range(max_pt), blks, inc_mat=None, name=nm )
-    #if BD.is_block_design():
-    #    return BD
-    #else:
-    #    raise TypeError("parameters are not those of a block design.")
-    return BD
+    if not(test):
+        return BD
+    else:
+        pars = BD.parameters()
+        if BD.is_block_design(pars[0],pars[1],pars[2],pars[3]):
+            return BD
+        else:
+            raise TypeError("parameters are not those of a block design.")
