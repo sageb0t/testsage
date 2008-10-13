4c20d8d4de2fd7aab9fa424c87fd81c432835a08
Robert Bradshaw <robertwb@math.washington.edu>
2008-10-13 13:08:48 -0700

    [mq]: symbolic_minpoly

--- src/sage/rings/qqbar.py
+++ src/sage/rings/qqbar.py
@@ -546,6 +546,14 @@ class AlgebraicRealField(_uniq_alg_r, AlgebraicField_common):
         else:
             raise NotImplementedError
 
+    def algebraic_closure(self):
+        """
+        EXAMPLES:
+            sage: AA.algebraic_closure()
+            Algebraic Field
+        """
+        return QQbar
+
     def _is_valid_homomorphism_(self, codomain, im_gens):
         try:
             return im_gens[0] == codomain._coerce_(self.gen(0))
@@ -697,6 +705,14 @@ class AlgebraicField(_uniq_alg, AlgebraicField_common):
             return create_RealField(prec, **extras).complex_field()
         else:
             raise NotImplementedError
+
+    def algebraic_closure(self):
+        """
+        EXAMPLES:
+            sage: QQbar.algebraic_closure()
+            Algebraic Field
+        """
+        return self
 
     def gens(self):
         return(QQbar_I, )
