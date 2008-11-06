99af4f23b30f6c6be9c20da59326a626cd85d36c
Mike Hansen <mhansen@gmail.com>
2008-11-06 04:24:41 -0800

    Add symbolic factorial and gamma functions.

--- src/sage/functions/all.py
+++ src/sage/functions/all.py
@@ -1,7 +1,7 @@
 from piecewise import piecewise, Piecewise
 
 from transcendental import (exponential_integral_1,
-                            gamma, gamma_inc, incomplete_gamma,
+                            gamma_inc, incomplete_gamma,
                             zeta, zeta_symmetric,
                             Li, Ei,
                             prime_pi,
