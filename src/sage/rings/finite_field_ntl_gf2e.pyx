80713d04264dd5196d8902bee2f36a83d95fd5d9
Yann Laigle-Chapuy <yannlaiglechapuy@gmail.com>
2009-01-24 16:15:19 +0100

    trac #5088 - Pohlig-Hellman

--- src/sage/rings/finite_field_ntl_gf2e.pyx
+++ src/sage/rings/finite_field_ntl_gf2e.pyx
@@ -1414,9 +1414,8 @@ cdef class FiniteField_ntl_gf2eElement(FiniteFieldElement):
         """
         from  sage.groups.generic import discrete_log
 
-        q = Integer((self.parent()).order())
         b = self.parent()(base)
-        return discrete_log(self, b, q-1)
+        return discrete_log(self, b)
 
 def unpickleFiniteField_ntl_gf2eElement(parent, elem):
     """
