20597053744f094aa85f880a262162d048d38306
Mike Hansen <mhansen@gmail.com>
2009-02-14 20:02:08 -0800

    Fixed #5257: Clicking on "Log" in the notebook (bringing you to the /history page) gives errors

--- src/sage/server/notebook/twist.py
+++ src/sage/server/notebook/twist.py
@@ -1584,8 +1584,10 @@ class History(resource.Resource):
         self.username = username
 
     def render(self, ctx):
-        s = notebook.user_history_html(self.username)
-        return http.Response(stream=s)
+        t = template('history.html', username=self.username,
+                     text = notebook.user_history_text(self.username),
+                     actions=False)
+        return http.Response(stream=t)
 
 class LiveHistory(resource.Resource):
     def __init__(self, username):
