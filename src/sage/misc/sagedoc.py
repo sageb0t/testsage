676c17c230384bdda6d0da66bf2f564fb878b0ba
J. H. Palmieri <palmieri@math.washington.edu>
2009-03-16 20:15:32 -0700

    fix searching: wrong URLs and missing search results

--- src/sage/misc/sagedoc.py
+++ src/sage/misc/sagedoc.py
@@ -251,7 +251,7 @@ def format_search_as_html(what, r, search):
     s += '<ol>'
 
     files = set([])
-    for L in r.splitlines()[4:]:
+    for L in r.splitlines():
         i = L.find(':')
         if i != -1:
             files.add(L[:i])
@@ -259,6 +259,7 @@ def format_search_as_html(what, r, search):
     files.sort()
     for F in files:
         if F.endswith('.html'):
+            F = F.split('/', 2)[2]
             url = '/doc/live/' + F
         else:
             # source code
