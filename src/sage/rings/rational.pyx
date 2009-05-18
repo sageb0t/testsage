2278a3194eaa93b8b1c8fb96c2078d3f7fba1043
Gonzalo Tornaría <tornaria@math.utexas.edu>
2009-05-17 22:42:53 -0700

    #6062: QQ method is_padic_square, allow python int argument

--- src/sage/rings/rational.pyx
+++ src/sage/rings/rational.pyx
@@ -946,6 +946,10 @@ cdef class Rational(sage.structure.element.FieldElement):
             True
             sage: QQ(2).is_padic_square(5)
             False
+
+        TESTS:
+            sage: QQ(5/7).is_padic_square(int(2))
+            False
         """
         ## Special case when self is zero
         if self.is_zero():
@@ -957,6 +961,7 @@ cdef class Rational(sage.structure.element.FieldElement):
             return (self > 0)
 
         ## Check that p is prime
+        p = ZZ(p)
         if not p.is_prime():
             raise ValueError, 'p must be "infinity" or a positive prime number.'
 
