10d97522c1d506261abad016f1a13516adabb0cf
Benjamin Antieau <antieau@math.uic.edu>
2009-11-06 15:16:06 -0600

    This is the hopefully final patch for ticket #6099, which implements morphisms of simplicial complexes.

--- src/sage/categories/category_types.py
+++ src/sage/categories/category_types.py
@@ -1238,6 +1238,7 @@ category_hierarchy = {\
     Objects                : [Sets],\
     Sets                   : [],\
     GSets                  : [Sets],\
+    SimplicialComplexes    : [Sets],\
     Semigroups             : [Sets],\
     Monoids                : [Semigroups, Sets],\
     Groups                 : [Monoids, Semigroups, Sets],\
@@ -1255,6 +1256,7 @@ category_hierarchy = {\
     GroupAlgebras          : [MonoidAlgebras, Algebras, Sets],\
     MatrixAlgebras         : [Algebras, Sets],\
     RingModules            : [AbelianGroups, Sets],\
+    ChainComplexes         : [],\
     FreeModules            : [RingModules, AbelianGroups, Sets],\
     VectorSpaces           : [FreeModules, RingModules, AbelianGroups, Sets],\
     HeckeModules           : [FreeModules, RingModules, AbelianGroups, Sets],\
