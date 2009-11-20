337c9aa0f3b07e911784b548fa8c9fade3b905cd
Robert Bradshaw <robertwb@math.washington.edu>
2009-11-19 20:28:06 -0800

    Follow-up to #6496, make denominator live in basering.

--- src/sage/rings/polynomial/multi_polynomial.pyx
+++ src/sage/rings/polynomial/multi_polynomial.pyx
@@ -1091,8 +1091,6 @@ cdef class MPolynomial(CommutativeRingElement):
         """
         if self.degree() == -1:
             return 1
-        #R was defined for univariate polynomials. Is it useless?
-        #R = self.base_ring()
         x = self.coefficients()
         try:
             d = x[0].denominator()
@@ -1100,7 +1098,7 @@ cdef class MPolynomial(CommutativeRingElement):
                 d = d.lcm(y.denominator())
             return d
         except(AttributeError):
-            return self.parent().one_element()
+            return self.parent().base_ring().one_element()
 
     def numerator(self):
         """
