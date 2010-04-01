f86b89bcebb6fc709f2e9092288f134175ba3698
Nathann Cohen <nathann.cohen@gmail.com>
2010-04-01 16:06:55 +0200

    trac #8639 : a missing _ in a viarble's name

--- src/sage/numerical/mip.pyx
+++ src/sage/numerical/mip.pyx
@@ -758,7 +758,7 @@ class MixedIntegerLinearProgram:
             sage: p.set_real(x[3])
         """
         if isinstance(e, MIPVariable):
-            e.vtype = self.__BINARY
+            e._vtype = self.__BINARY
             if e.depth() == 1:
                 for v in e.values():
                     self._variables_type[self._variables[v]] = self.__BINARY
@@ -826,7 +826,7 @@ class MixedIntegerLinearProgram:
             sage: p.set_real(x[3])
         """
         if isinstance(e, MIPVariable):
-            e.vtype = self.__INTEGER
+            e._vtype = self.__INTEGER
             if e.depth() == 1:
                 for v in e.values():
                     self._variables_type[self._variables[v]] = self.__INTEGER
@@ -894,7 +894,7 @@ class MixedIntegerLinearProgram:
             sage: p.set_binary(x[3])
         """
         if isinstance(e, MIPVariable):
-            e.vtype = self.__REAL
+            e._vtype = self.__REAL
             if e.depth() == 1:
                 for v in e.values():
                     self._variables_type[self._variables[v]] = self.__REAL
