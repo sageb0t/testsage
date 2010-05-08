5f1e160d72edc43dea62053c987ad6a98907857c
Tim Joseph Dumol <tim@timdumol.com>
2010-05-07 21:04:35 -0700

    #5405 - create a decorator for adding default keyword arguments to a function.

--- src/sage/misc/all.py
+++ src/sage/misc/all.py
@@ -170,6 +170,8 @@ from explain_pickle import explain_pickle, unpickle_newobj, unpickle_global, unp
 
 from bitset import FrozenBitset, Bitset
 
+from decorators import specialize
+
 ##########################################################################
 def benchmark(n=-1):
     """
