74271b86ee50ba72c6a29b7b8b083e667ba7c372
Andrey Novoseltsev <novoselt@gmail.com>
2010-05-18 03:20:32 -0600

    Trac 8989: Add support for Fano toric varieties.

--- src/sage/schemes/generic/all.py
+++ src/sage/schemes/generic/all.py
@@ -4,6 +4,7 @@ from spec             import Spec, is_Spec
 from affine_space     import AffineSpace, is_AffineSpace
 from algebraic_scheme import is_AlgebraicScheme
 from ambient_space    import is_AmbientSpace
+from fano_toric_variety import CPRFanoToricVariety
 from morphism         import is_SchemeMorphism
 from projective_space import ProjectiveSpace, is_ProjectiveSpace
 from scheme           import is_Scheme, is_AffineScheme
