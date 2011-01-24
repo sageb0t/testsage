95901c442fc5a0698208555878cf730b4cf2bd20
Martin Albrecht <martinralbrecht@googlemail.com>
2011-01-24 23:30:59 +0000

    Trac #1819: #1819 move sage.crypto.mq.MPolynomialSystem to sage.rings.polynomial.multi_polynomial_system.PolynomialSystem and adapt return types of various multivariate polynomial ideal methods

--- src/sage/rings/number_field/order.py
+++ src/sage/rings/number_field/order.py
@@ -1308,7 +1308,7 @@ class AbsoluteOrder(Order):
         The basis is an immutable sequence::
 
             sage: type(O.basis())
-            <class 'sage.structure.sequence.Sequence'>
+            <class 'sage.structure.sequence.Sequence_generic'>
 
         The generator functionality uses the basis method::
 
