7371476af1ce3c543728b9ca79e4529d1b38f310
Jean-Pierre Flori <flori@enst.fr>
2011-02-14 14:06:15 +0100

    Trac #7377: Split Expect interface.

--- src/sage/interfaces/gap.py
+++ src/sage/interfaces/gap.py
@@ -619,7 +619,7 @@ class Gap_generic(Expect):
 
             sage: s = gap.function_call('Display', [gap.SymmetricGroup(5).CharacterTable()])
             sage: type(s)
-            <class 'sage.interfaces.expect.AsciiArtString'>
+            <class 'sage.interfaces.interface.AsciiArtString'>
             sage: s.startswith('CT')
             True
         """
