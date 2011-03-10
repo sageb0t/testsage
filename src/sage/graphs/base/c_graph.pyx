3a0c4c7319b4f60aad4915e93a6aaf0e9bccdecb
YLC <y@l.c>
2011-03-10 23:10:50 +0100

    Trac #10885: #10885 correct small bug in reviewers patch

--- src/sage/graphs/base/c_graph.pyx
+++ src/sage/graphs/base/c_graph.pyx
@@ -2995,6 +2995,7 @@ def floyd_warshall(gg, paths = True, distances = False):
         if paths: tmp_prec = {}
         if distances: tmp_dist = {}
         v = vertex_label(v_int, ggbvi, ggbvl, g)
+        dv = dist[v_int]
         for u_int in gverts:
             u = vertex_label(u_int, ggbvi, ggbvl, g)
             if paths:
