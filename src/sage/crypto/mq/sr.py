c5dec75b69b630a163a82a439ae960057f0f3fd0
Andrey Novoseltsev <novoselt@gmail.com>
2011-08-17 17:37:04 -0400

    Trac #10793: Fix wrong doctests that were working due to a bug in matrix constructor.

--- src/sage/crypto/mq/sr.py
+++ src/sage/crypto/mq/sr.py
@@ -1001,7 +1001,7 @@ class SR_generic(MPolynomialSystemGenerator):
 
         if is_Matrix(d):
             if d.nrows() == r*c*e:
-                return Matrix(k, c, r, self.antiphi(d)).transpose()
+                return Matrix(k, c, r, self.antiphi(d).list()).transpose()
             elif d.ncols() == c and d.nrows() == r and d.base_ring() == k:
                 return d
 
@@ -2099,7 +2099,7 @@ class SR_gf2n(SR_generic):
         if d is None:
             return Matrix(k, r*c*e, 1)
         elif d.ncols() == c and d.nrows() == r and d.base_ring() == k:
-            return Matrix(k, r*c*e, 1, self.phi(d).transpose())
+            return Matrix(k, r*c*e, 1, self.phi(d).transpose().list())
 
     def is_vector(self, d):
         """
