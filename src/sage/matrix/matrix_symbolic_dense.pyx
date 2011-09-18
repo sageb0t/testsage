06e0920bbab7ab3765c278d0f94449083124cf3d
Harald Schilly <harald.schilly@gmail.com>
2011-09-17 21:12:17 -0700

    Trac #8094: 8094: basic properties for matrices

--- src/sage/matrix/matrix_symbolic_dense.pyx
+++ src/sage/matrix/matrix_symbolic_dense.pyx
@@ -59,6 +59,17 @@ Computing inverses::
     ...
     ArithmeticError: self must be a square matrix
 
+Transposition::
+    sage: m = matrix(SR, 2, [sqrt(2), -1, pi, e^2])
+    sage: m.transpose()
+    [sqrt(2)      pi]
+    [     -1     e^2]
+
+``.T`` is a convenient shortcut::
+    sage: m.T
+    [sqrt(2)      pi]
+    [     -1     e^2]
+
 Test pickling::
 
     sage: m = matrix(SR, 2, [sqrt(2), 3, pi, e]); m
