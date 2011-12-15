745a580ed1b31e9dbda0c1db2ad87900596b4010
Volker Braun <vbraun@stp.dias.ie>
2011-12-15 19:43:40 +0000

    Trac #12159: Trac #12159: Placing triangulation and normal cones
    This patch renames triangulation.py -> element.py since
    all files are in the sage/geometry/triangulation directory.

--- src/sage/geometry/triangulation/base.pyx
+++ src/sage/geometry/triangulation/base.pyx
@@ -603,7 +603,7 @@ cdef class ConnectedTriangulationsIterator(SageObject):
       triangulation as
 
         * A
-          :class:`~sage.geometry.triangulation.point_configuration.Triangulation`
+          :class:`~sage.geometry.triangulation.element.Triangulation`
           object, or
 
         * an iterable of iterables specifying the vertices of the simplices, or
@@ -625,7 +625,7 @@ cdef class ConnectedTriangulationsIterator(SageObject):
     An iterator. The generated values are tuples of
     integers, which encode simplices of the triangulation. The output
     is a suitable input to
-    :class:`~sage.geometry.triangulation.point_configuration.Triangulation`.
+    :class:`~sage.geometry.triangulation.element.Triangulation`.
 
     EXAMPLES::
 
@@ -647,7 +647,7 @@ cdef class ConnectedTriangulationsIterator(SageObject):
 
     You can reconstruct the triangulation from the compressed output via::
 
-        sage: from sage.geometry.triangulation.point_configuration import Triangulation
+        sage: from sage.geometry.triangulation.element import Triangulation
         sage: Triangulation((2, 3, 4, 5), p)
         (<0,1,3>, <0,1,4>, <0,2,3>, <0,2,4>)
 
