145edf1b0fd020898987495a64f6b5180e85aca0
Nils Bruin <nbruin@sfu.ca>
2012-03-05 22:43:34 -0800

    Trac #12633: #12633: Fix documentation of attach

--- src/sage/misc/session.pyx
+++ src/sage/misc/session.pyx
@@ -409,8 +409,10 @@ def attach(*files):
        ``from m import *`` is executed.
 
     Type ``attached_files()`` for a list of all currently
-    attached files. You can remove files from this list to stop them
-    from being watched.
+    attached files.
+
+    Use ``detach(...)`` to instruct Sage to remove a file from the internal
+    list watched files.
 
     .. note::
 
