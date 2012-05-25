a6617d071852694660d9ce5a3f6c95ffe059aec0
Karl-Dieter Crisman <kcrisman@gmail.com>
2012-05-25 11:48:30 -0400

    Trac #12845: Trac 12845 reviewer patch

--- src/sage/misc/functional.py
+++ src/sage/misc/functional.py
@@ -1160,6 +1160,7 @@ def norm(x):
         sage: v = vector([a, b, c])
         sage: bool(norm(v).simplify() == sqrt(a^2 + b^2 + c^2))
         True
+        sage: forget()
     """
     return x.norm()
 
