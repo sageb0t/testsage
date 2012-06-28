8611da70c42949fe60bb8ee5f82c01ba717c4d11
David Roe <roed.math@gmail.com>
2012-06-28 01:40:27 -0400

    Trac #13172: #13172: fixes a bug in integer_mod's nth_root function.

--- src/sage/rings/finite_rings/integer_mod.pyx
+++ src/sage/rings/finite_rings/integer_mod.pyx
@@ -1075,6 +1075,11 @@ cdef class IntegerMod_abstract(FiniteRingElement):
             ...       except ValueError:
             ...           pass
 
+        We check that #13172 is resolved::
+
+            sage: mod(-1, 4489).nth_root(2, all=True)
+            []
+
         ALGORITHMS:
 
         - The default for prime modulus is currently an algorithm described in the following paper:
@@ -1152,7 +1157,7 @@ cdef class IntegerMod_abstract(FiniteRingElement):
                 else:
                     return K(p**(pval // n) * mod(upart, p**(k-pval)).nth_root(n, algorithm=algorithm).lift())
             from sage.rings.padics.all import ZpFM
-            R = ZpFM(p,k,print_mode='digits')
+            R = ZpFM(p,k)
             self_orig = self
             if p == 2:
                 sign = [1]
